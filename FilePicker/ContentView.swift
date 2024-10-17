4be25d1374804f5f58b2061757535de3397841e9
RodrigoArturoFG <rodrigoarturofg@gmail.com>
2024-10-17 10:54:22 -0600

    Se agregó Documentación

--- FilePicker/ContentView.swift
+++ FilePicker/ContentView.swift
@@ -12,11 +12,14 @@ struct ContentView: View {
     
     // MARK: Variables necesarias en la vista para el metodo Seleccionar Imagen
     @State private var isPhotoPickerPresented: Bool = false
-    @State private var inputData: Data?
-    //@State private var inputImage: UIImage?
-    
+    @State private var inputImage: UIImage?
+    //@State private var inputData: Data?
+
     @State private var presentImageAlert: Bool = false
     @State private var messageImageAlert = "Ocurrio un error al cargar la imagen"
+    
+    // MARK: PlaceHolder de la imagen
+    @State private var image = Image(systemName:"photo.on.rectangle.angled")
     
     // MARK: Variables necesarias en la vista para el metodo Seleccionar documentos (PDF)
     @State private var isFilePickerPresented: Bool = false
@@ -27,22 +30,26 @@ struct ContentView: View {
     @State private var presentDocumentAlert: Bool = false
     @State private var messageDocumentAlert = "Ocurrio un error al cargar el documento"
     
-    // MARK: PlaceHolder de la imagen
-    @State private var image = Image("AppCDMXDummy")
-    
     var body: some View {
         VStack {
             
+            // MARK: Seleccionar Imagen
+            
+            // Al usar PHPicker, No se requiere solicitar al usuario el acceso a su biblioteca de fotografías.
+            // De acuerdo con Apple: "It has privacy built in by default. It doesn't need direct access to the photos library to show the picker and the picker won't prompt for access on behalf of the app." - https://developer.apple.com/videos/play/wwdc2020/10652/
+            
             Button("Seleccionar Imagen") {
-                // Al usar PHPicker, No se requiere acceso directo a la biblioteca de fotografías del usuario. De acuerdo con Apple: "It has privacy built in by default. It doesn't need direct access to the photos library to show the picker and the picker won't prompt for access on behalf of the app." - https://developer.apple.com/videos/play/wwdc2020/10652/
                 isPhotoPickerPresented.toggle()
             }.sheet(isPresented: $isPhotoPickerPresented) {
-                PhotoPicker(isPhotoPickerPresented: $isPhotoPickerPresented, data: $inputData, presentImageAlert: $presentImageAlert)
-            }.onChange(of: inputData) { _ in
+                PhotoPicker(isPhotoPickerPresented: $isPhotoPickerPresented, image:$inputImage, presentImageAlert: $presentImageAlert)
+            }.onChange(of: inputImage) { _ in
                 presentImageAlert = false
+                guard let inputImage = inputImage else { return }
+                image = Image(uiImage: inputImage)
+                /*
                 guard let inputData = inputData else { return }
                 guard let imageParsed = UIImage(data: inputData) else { return }
-                image = Image(uiImage: imageParsed)
+                image = Image(uiImage: imageParsed)*/
             }.alert(isPresented: $presentImageAlert) {
                 Alert(
                     title: Text("Error Imagen"),
@@ -53,22 +60,32 @@ struct ContentView: View {
                 )
             }.padding()
             
-            // Image View - Contenedor de la imágen seleccionada
+            // MARK: Image View - Contenedor de la imágen seleccionada
             image.resizable()
                 .scaledToFill()
                 .frame(width: 107, height: 107)
                 .padding()
             
+            // MARK: Seleccionar Documento
+            
+            // Al usar UIDocumentPickerViewController, tampoco se requiere solicitar al usuario el acceso al directorio del sipositivo.
+            // De acuerdo con Apple: In iOS 13, users can select a directory from any of the available file providers using a UIDocumentPickerViewController. The document picker returns a security-scoped URL for the directory that permits your app to access content outside its container.
+            // When the user selects a directory in the document picker, the system gives your app permission to access that directory and all of its contents. The document picker returns a security-scoped URL for the directory. - https://developer.apple.com/documentation/uikit/view_controllers/providing_access_to_directories
+            
             Button("Seleccionar Documentos") {
                 isFilePickerPresented.toggle()
             }.sheet(isPresented: $isFilePickerPresented) {
+                // Presentar el Document Picker
                 DocumentPicker(isFilePickerPresented: $isFilePickerPresented, documentData: $documentData, presentDocumentAlert: $presentDocumentAlert)
             }.onChange(of: documentData) { _ in
+                // Se obtuvo la Data del documento seleccionado
+                // Manejo de la respuesta
                 let bcf = ByteCountFormatter()
-                bcf.allowedUnits = [.useMB] // optional: restricts the units to MB only
+                bcf.allowedUnits = [.useKB] // optional: restricts the units to MB only
                 bcf.countStyle = .file
                 self.documentSize = bcf.string(fromByteCount: Int64(documentData!.count))
             }.alert(isPresented: $presentDocumentAlert) {
+                // Ocurrió un error al seleccionar un documento
                 Alert(
                     title: Text("Error Documento"),
                     message: Text(messageDocumentAlert),
@@ -78,7 +95,7 @@ struct ContentView: View {
                 )
             }.padding()
             
-            Text("Data: " + (self.documentSize ?? "0 MB"))
+            Text("Data: " + (self.documentSize ?? "0 KB"))
                 .foregroundColor(.secondary)
         }
         .padding()
--- FilePicker/DocumentPicker/DocumentPicker.swift
+++ FilePicker/DocumentPicker/DocumentPicker.swift
@@ -7,13 +7,71 @@
 
 import SwiftUI
 
+/** Custom UIViewControllerRepresentable de UIDocumentPickerViewController - iOS 14+
+ 
+Nos permite mostrarle al usuario los archivos de su dispositivo (solo archivos .pdf), para seleccionar el archivo deseado.
+ El archivo es devuelto en formato **Data** para su posterior manipulación en la aplicación.
+ 
+ **No se requiere solicitar al usuario el acceso al directorio del dipositivo.**
+ De acuerdo con Apple: *When the user selects a directory in the document picker, the system gives your app permission to access that directory and all of its contents. The document picker returns a security-scoped URL for the directory.*
+ 
+ https://developer.apple.com/documentation/uikit/view_controllers/providing_access_to_directories
+   
+   
+**Uso (Declaration con variables Bindings)**
+ ````
+ DocumentPicker(isFilePickerPresented: Binding<Bool>, documentData: Binding<Data?>, presentDocumentAlert: Binding<Bool>)
+ ````
+ 
+**Proceso**
+ 1. Presentar el Picker -> isFilePickerPresented = true
+ 2. El usuario selecciona el documento PDF deseado
+ 3. Se trata el documento para convertirlo en Data
+ 4. Se devuelve la Data ->  documentData = Data
+ 6. Si ocurre un error se devuelve un true ->  presentDocumentAlert = true
+ 5. Despedir la hoja presentada
+ 
+ *Ejemplo de uso en SwuiftUI*
+ 
+ ````
+ // Declarar variables antes del body
+ @State private var isFilePickerPresented: Bool = false
+ @State private var documentData: Data?
+ @State private var documentSize: String?
+ 
+ @State private var presentDocumentAlert: Bool = false
+ @State private var messageDocumentAlert = "Ocurrio un error al cargar el documento"
+ 
+ // Dentro del body
+ Button("Seleccionar Imagen") {
+     isPhotoPickerPresented.toggle()
+ }.sheet(isPresented: $isPhotoPickerPresented) {
+     PhotoPicker(isPhotoPickerPresented: $isPhotoPickerPresented, image:$inputImage, presentImageAlert: $presentImageAlert)
+ }.onChange(of: inputImage) { _ in
+     presentImageAlert = false
+     guard let inputImage = inputImage else { return }
+     image = Image(uiImage: inputImage)
+ }.alert(isPresented: $presentImageAlert) {
+     Alert(
+         title: Text("Error Imagen"),
+         message: Text(messageImageAlert),
+         dismissButton: .default(Text("Ok"), action: {
+             presentImageAlert.toggle()
+         })
+     )
+ }
+ ````
+ 
+ - Version: 1.0
+ 
+ */
 struct DocumentPicker: UIViewControllerRepresentable {
     @Binding var isFilePickerPresented: Bool
     @Binding var documentData: Data?
     //@Binding var filePath: URL?
     
     @Binding var presentDocumentAlert: Bool
-
+    
     func makeUIViewController(context: Context) -> UIDocumentPickerViewController {
         let controller = UIDocumentPickerViewController(forOpeningContentTypes: [.pdf])
         controller.allowsMultipleSelection = false
@@ -43,16 +101,28 @@ struct DocumentPicker: UIViewControllerRepresentable {
             
             parent.isFilePickerPresented = false
             filePath = urls[0]
-            print(urls[0].absoluteString)
+            //print(urls[0].absoluteString)
                         
             if (filePath != nil) {
+                // Start accessing the content's security-scoped URL.
+                guard filePath!.startAccessingSecurityScopedResource() else {
+                    // Handle the failure here.
+                    // Ocurrió un error al acceder al documento
+                    self.parent.presentDocumentAlert = true
+                    return
+                }
+                
                 do {
                     let fileData = try Data(contentsOf: filePath! as URL)
                     print(fileData)
                     self.parent.documentData = fileData
+                    // Make sure you release the security-scoped resource when you finish.
+                    filePath!.stopAccessingSecurityScopedResource()
                 } catch {
-                    print("No se pueden cargar los datos: \(error)")
+                    // Ocurrió un error al convertir el documento en Data
                     self.parent.presentDocumentAlert = true
+                    // Make sure you release the security-scoped resource when you finish.
+                    filePath!.stopAccessingSecurityScopedResource()
                 }
             }
         }
